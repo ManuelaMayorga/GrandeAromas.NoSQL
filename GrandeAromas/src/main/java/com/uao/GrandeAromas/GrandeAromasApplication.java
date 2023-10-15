52b22df77297f3a8d08c4094d81fbc64f52504e0
ManuelaMayorga1 <manuelamayorga486@gmail.com>
2023-10-15 12:30:38 -0500

    15/10/2023

---
+++ GrandeAromas/src/main/java/com/uao/GrandeAromas/GrandeAromasApplication.java
@@ -0,0 +1,13 @@
+package com.uao.GrandeAromas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrandeAromasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrandeAromasApplication.class, args);
+	}
+
+}
